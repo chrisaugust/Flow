ad72bdc04741d4d2fda584bbb13c31488217fa18
Chris Danielson <christopher.a.danielson@protonmail.com>
2023-05-03 06:40:23 -0600

    Fix bug for current month method in model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -9,10 +9,10 @@ class Expense < ApplicationRecord
   validates :category, presence: true
   
   def self.current_month
-    Expense.find_by_sql(["SELECT * FROM expenses WHERE date >= ?", Date.today.last_month.end_of_month])
+    Expense.find_by_sql(["SELECT * FROM expenses WHERE date > ?", Date.today.last_month.end_of_month])
   end
 
   def self.current_month_for(user_id)
-    Expense.find_by_sql(["SELECT * FROM expenses WHERE date >= ? AND user_id = ?", Date.today.last_month.end_of_month, user_id]) 
+    Expense.find_by_sql(["SELECT * FROM expenses WHERE date > ? AND user_id = ?", Date.today.last_month.end_of_month, user_id]) 
   end
 end
