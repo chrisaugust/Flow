cc322d406caf25f4f8d4c2d4dcf8a37907304a06
Christopher Danielson <christopher.a.danielson@protonmail.com>
2023-07-25 09:20:09 -0400

    Refactor categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,98 +2,55 @@ class CategoriesController < ApplicationController
   
   before_action :require_signin, except: [:home]
   before_action :require_admin, only: [:edit, :update, :destroy]
-
+  before_action :set_categories, only: [:home, :index, :current_month, :single_month]
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  
   def home 
     @expenses = Expense.all
-
-    @categories = Category.all
-
-    @totals_by_category = @categories.each_with_object({}) { |cat, hash| hash[cat] = 0 }
-    @expenses.each { |e| @totals_by_category[e.category] += e.amount }
-
-    @total_spending = total(@expenses)
-
-    @chart_data = @categories.map { |cat| cat.name }.zip(@totals_by_category.values)   
-
+    set_chart_data
     render :home
   end
 
   def index
-    if current_user 
-      @expenses = current_user.expenses
-    else
-      @expenses = Expense.all
-    end
-
-    @categories = current_user.categories.uniq
-
-    @totals_by_category = @categories.each_with_object({}) { |cat, hash| hash[cat] = 0 }
-    @expenses.each { |e| @totals_by_category[e.category] += e.amount }
-
-    @total_spending = total(@expenses)
-
-    @chart_data = @categories.map { |cat| cat.name }.zip(@totals_by_category.values)   
+    @expenses = current_user ? current_user.expenses : Expense.all
+    set_chart_data
   end
 
   def current_month
-    if current_user 
-      @expenses = Expense.current_month_for(current_user)
-      @categories = current_user.categories.uniq
-    else
-      @expenses = Expense.current_month
-      @categories = Category.all
-    end 
-
-    @totals_by_category = @categories.each_with_object({}) { |cat, hash| hash[cat] = 0 } 
-    @expenses.each { |e| @totals_by_category[e.category] += e.amount }
-
-    @total_spending = total(@expenses)
-
-    @chart_data = @categories.map { |cat| cat.name }.zip(@totals_by_category.values)   
+    @expenses = current_user ? 
+      Expense.current_month_for(current_user) : 
+      Expense.current_month
+    set_chart_data
   end
 
   def past_months
-    if current_user
-      @months = Expense.past_three_months_for(current_user)
-    else
-      redirect_to new_session_url
-    end
+    redirect_to new_session_url unless current_user
+    @months = Expense.past_three_months_for(current_user)
   end
 
   def single_month
     month_year = params["format"]
     @month, @year = month_year.split('-')
-
-    if current_user 
-      @expenses = Expense.expenses_for(month_year, current_user)
-      @categories = current_user.categories.uniq
-    else
-      redirect_to new_session_url
-    end 
-
-    @totals_by_category = @categories.each_with_object({}) { |cat, hash| hash[cat] = 0 } 
-    @expenses.each { |e| @totals_by_category[e.category] += e.amount }
-
-    @total_spending = total(@expenses)
-
-    @chart_data = @categories.map { |cat| cat.name }.zip(@totals_by_category.values)   
+    redirect_to new_session_url unless current_user
+    @expenses = Expense.expenses_for(month_year, current_user)
+    set_chart_data
   end
 
   def show
-    @category = Category.find(params[:id])
 
-    if current_user
-      @expenses = @category.expenses.order(date: :desc).filter { |e| e.user == current_user }
-    else
-      @expenses = @category.expenses.order(date: :desc)
-    end
+    # if current_user
+    #   @expenses = @category.expenses.order(date: :desc).filter { |e| e.user == current_user }
+    # else
+    #   @expenses = @category.expenses.order(date: :desc)
+    # end
 
+    @expenses = @category.expenses.order(date: :desc)
+      .filter { |e| e.user == current_user }
     @total_spending = total(@expenses)
+    @spending_by_date = @expenses.group_by(&:date)
+      .transform_values { |expenses| expenses.sum(&:amount) }
 
-    @spending_by_date = @expenses.group_by(&:date).
-      transform_values { |expenses| expenses.sum(&:amount) }
-
-    @chart_data = @spending_by_date
+    #@chart_data = @spending_by_date
   end
 
   def new
@@ -110,11 +67,9 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
     if @category.update(category_params)
       redirect_to category_path(@category), notice: "Category updated!"
     else
@@ -123,7 +78,6 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id]) 
     @category.destroy
     redirect_to categories_url, status: :see_other
   end
@@ -139,4 +93,18 @@ class CategoriesController < ApplicationController
       expense_records.map { |record| record.amount }.reduce(:+) || 0
     end
 
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def set_categories
+      @categories = current_user ? current_user.categories.uniq : Category.all
+    end
+
+    def set_chart_data
+      @totals_by_category = @categories.each_with_object({}) { |cat, hash| hash[cat] = 0 }
+      @expenses.each { |e| @totals_by_category[e.category] += e.amount }
+      @total_spending = total(@expenses)
+      @chart_data = @categories.map { |cat| cat.name }.zip(@totals_by_category.values)
+    end
 end
